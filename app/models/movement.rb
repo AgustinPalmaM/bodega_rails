5fcacc2379c54110362e012223f9cc6ac46de85f
AgustinPalmaM <palma.agustin7@gmail.com>
2022-09-09 16:40:12 -0400

    Create form to create new movements, add route create movement with post method in routes.rb

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,10 +1,16 @@
 class Movement < ApplicationRecord
   belongs_to :product
-  MovementTypes = {
-    add:0, 
+  MOVEMENT_TYPES = {
+    add: 0,
     remove: 1
   }
 
-  validates :quantity, presence: true, numericality: true
+  def self.get_movement_types
+    {
+      'Agregar producto' => MOVEMENT_TYPES[:add], 
+      'Quitar producto' => MOVEMENT_TYPES[:remove]
+    }
+  end
 
+  validates :quantity, presence: true, numericality: true
 end
