c7648e4377cc8b58bbcc94523e14add422c86439
AgustinPalmaM <palma.agustin7@gmail.com>
2022-09-09 18:37:20 -0400

    Create method to show total stock of a product, movement type add is plus a quantiry and movement type remove is less a quantity

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
   validates :name, :reference, :description, presence: true
   has_many :movements
+
+  def quantity_stock
+    total = 0
+    movements.each do |movement|
+      if movement.movement_type == Movement::MOVEMENT_TYPES[:add]
+        total += movement.quantity
+      else
+        total -= movement.quantity
+      end
+    end
+    total
+  end
 end
