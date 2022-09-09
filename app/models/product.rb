f3ebc2808abef0e61af4515dcb65c398a09bfe88
AgustinPalmaM <palma.agustin7@gmail.com>
2022-09-09 15:55:15 -0400

    Create movement model and links to access new movement view from show product, add button "Nuevo movimiento"

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   validates :name, :reference, :description, presence: true
+  has_many :movements
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "dashboard#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :products
+  resources :products do
+    member do
+      get :new_movement
+    end
+  end
 
 end
